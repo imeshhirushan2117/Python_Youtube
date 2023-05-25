5070460ae89dd883ea6b43f82445fc99ceec86fe
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-05-26 01:29:05 +0530

    PRINT AND INPUT(python tutorial 04)

---
+++ test.py
@@ -0,0 +1,25 @@
+# name = 'Imesh'
+# print("His name is : "+name)
+#
+# age = 22
+# print("His age is : " + str(age))
+#
+
+#=================== PRINT AND INPUT(python tutorial 04) ===================
+name = "Tisuri"
+age = 21
+print("her name is "+name + ". her age is "+str(age))
+
+print ('her name is %s and her age is %d ' %(name,age))
+
+x= input("Enter your name : ")
+print ("Your name is "+x)
+print (type(x))
+
+z = input("Enter Your Age : ")
+print("Your age is "+z)
+print (type(z))
+
+y = int(input("Enter Your Age : "))
+print("Your age is "+str(y))
+print (type(y))
